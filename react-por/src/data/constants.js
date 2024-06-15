97b21374c0521f1537b384fd0e2249fc7a113c47
Sheril <sherilmariamjose@gmail.com>
2024-06-16 00:22:40 +0530

    hero completed

---
+++ react-por/src/data/constants.js
@@ -0,0 +1,14 @@
+export const Bio = {
+    name: "Sheril Mariam Jose",
+    roles: [
+      "Full Stack Developer",
+      "Astrophile",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/sherilmariam/",
+    resume: "https://drive.google.com/file/d/1Ndw5AWDRdte0LEHNmP45LSW8QYiFcn6-/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/sheril-mariam-jose/",
+  };
+  
